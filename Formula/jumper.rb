7cdfecab592ee48b19055f21705e95b4538aa9dd
homerours <homerours@gmail.com>
2024-10-21 21:47:48 +0100

    add url + install

--- Formula/jumper.rb
+++ Formula/jumper.rb
@@ -2,9 +2,11 @@ class Jumper < Formula
   desc "Accurate and fast files/directories' jumper"
   homepage "https://github.com/homerours/jumper"
   license "MIT"
+  url "https://github.com/homerours/jumper/archive/refs/tags/v1.0.tar.gz"
   head "https://github.com/homerours/jumper", branch: "master"
 
   def install
-    system "make", "install"
+    system "make", "jumper"
+    bin.install "jumper"
   end
 end
