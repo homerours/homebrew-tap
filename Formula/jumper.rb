d32dc40218fa248ec65a9932e49080b501d014d5
homerours <homerours@gmail.com>
2024-10-21 21:52:04 +0100

    add sha256

--- Formula/jumper.rb
+++ Formula/jumper.rb
@@ -3,10 +3,12 @@ class Jumper < Formula
   homepage "https://github.com/homerours/jumper"
   license "MIT"
   url "https://github.com/homerours/jumper/archive/refs/tags/v1.0.tar.gz"
+  sha256 "c2c8ae1483be050d6456aca8159379885c0bf89f61889a3f2377fd6f21aef8cb"
   head "https://github.com/homerours/jumper", branch: "master"
 
   def install
     system "make", "jumper"
+    system "make", "clean"
     bin.install "jumper"
   end
 end
